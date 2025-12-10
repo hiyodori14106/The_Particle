924e43a1d2936c01d4d28a8b1885b635e74b150e
hiyodori14106 <246297538+hiyodori14106@users.noreply.github.com>
2025-12-10 15:56:09 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,9 @@
- let a = 0;
+let a = 0;
 function buttonClick(arg) {
     a++;
 }
-let button = document.getElementById('button1');
+let button = document.getElementById('button');
 button.addEventListener('click', function() {
     buttonClick(1);
-  document.getElementById("text").textContent = a;
+    document.getElementById("text").textContent = a;
 });
-  
-
-
