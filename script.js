8b9a02d7c9ff60a6ac8a420d5beb55f2aed1809a
hiyodori14106 <246297538+hiyodori14106@users.noreply.github.com>
2025-12-10 09:05:18 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,13 @@
-   document.addEventListener("DOMContentLoaded", function () {
-            let e = 1;
+document.addEventListener("DOMContentLoaded", function () {
+    let e = 1;
 
-            function a() {
-                const el = document.getElementById("text");
-                if (el) {
-                    el.textContent = e;
-                    e++;
-                }
-            }
+    function a() {
+        const el = document.getElementById("text");
+        if (el) {
+            el.textContent = e;
+            e++;
+        }
+    }
 
-            
-            a();
-
-            setInterval(a, 1000);
+    a();
+    setInterval(a, 1000);
