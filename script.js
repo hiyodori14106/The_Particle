8b6f0638049a43b7ad824424e49e9af8e8846027
hiyodori14106 <246297538+hiyodori14106@users.noreply.github.com>
2025-12-16 16:08:24 +0900

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ let a = 0;
 let e = 0;
 setInterval(() => {
  e = e+0.01
- a = e+a;
- document.getElementById("text").textContent = Mathfloor(a); 
+ a = Mathfloor(e+a);
+ document.getElementById("text").textContent = a; 
 },10)
